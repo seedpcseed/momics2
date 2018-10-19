3f5d2c134b9366f016c9a23e464f616e07ebf72d
PATRICK SEED <seedpcseed@gmail.com>
2018-10-19 15:41:49 -0500

    recoded to push lists of flags properly to commands

--- R/kneaddata.R
+++ R/kneaddata.R
@@ -40,23 +40,37 @@ kneadData_paired<-function(
   REFERENCE_DB=KNEADDATA_DB_HUMAN,
   OUTPUT="kneaddata",
   PATTERN1="R1",
-  PATTERN2="R2"
+  PATTERN2="R2",
+  ADD_ON=" --cat-final-output "
 ){
+  INPUT_FOLDER=file.path(getwd(), INPUT_FOLDER)
+  q<-sprintf("INPUT_FOLDER= %s", INPUT_FOLDER)
+  print(q)
   OUTPUT=file.path(getwd(), OUTPUT)
+  q<-sprintf("OUTPUT= %s", OUTPUT)
+  print(q)
   if(!dir.exists(OUTPUT)){system(paste0("mkdir ", OUTPUT))}
+
+  q<- sprintf("PATTERN1: %s and PATTERN2: %s", PATTERN1, PATTERN2)
+  print(q)
 
   R1=list.files(INPUT_FOLDER, pattern=PATTERN1, include.dirs=TRUE, full.names = TRUE)
   R2=list.files(INPUT_FOLDER, pattern=PATTERN2, include.dirs=TRUE, full.names = TRUE)
 
   R1<-sort(R1)
+  q<-sprintf("R1 FILES: %s", R1); print(q)
+
   R2<-sort(R2)
+  q<-sprintf("R2 FILES: %s", R2); print(q)
 
   for(i in 1:length(R1)){
-    code=paste0("kneaddata --input ", R1[i], "--input ", R2[i], " --reference-db ",
-                REFERENCE_DB, " --output ", OUTPUT)
+    code=paste0("kneaddata --input ", R1[i], " --input ", R2[i], " --reference-db ",
+                REFERENCE_DB, " --output ", OUTPUT, " ", ADD_ON)
     if(SINGULARITY){
       code=paste0("singularity exec -B ", SINGULARITY_B, " ", SINGULARITY_IMAGE, " ", code)
     }
+    q<-sprintf("Code: %s", code)
+    print(q)
     system(code)
   }
 }
--- R/utils.R
+++ R/utils.R
@@ -1,16 +1,19 @@
+library(rlist)
+
 str_eval=function(x) {return(eval(parse(text=x)))}
 
 do_function=function(x, calls){
   sprintf("Call: %s", x)
-  do.call(x, calls)
+  calls=make_list(calls)
+  do.call(str_eval(x), calls)
 }
 
 yaml_call<- function(x){
-  do_function(x[[1]], list(x[[2]]))
+  do_function(x[[1]], x[[2]])
 }
 
 
-  run_workflow<- function(yml="workflow.yml"){
+run_workflow<- function(yml="workflow.yml"){
     c<- read_yaml(yml)
     counter=length(c)
 
@@ -20,3 +23,13 @@ yaml_call<- function(x){
     }
   }
 
+make_list<-function(str_call){
+  df<-df<-data.frame(strsplit(str_call, "--"))
+  build_list<- list()
+  for(i in 2:nrow(df)){
+    call<- unlist(strsplit(as.character(df[i,1]), "="))
+    build_list<-list.append(build_list, assign(call[1], trimws(call[2])))
+    names(build_list)[length(build_list)]<-call[1]
+  }
+  return(build_list)
+}
